51d0f01ae9405d5f0995041e732617bb803ae6c6
foxane <65512187+foxane@users.noreply.github.com>
2024-08-26 22:15:55 +0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { Analytics } from '@vervel/analytics/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 );
